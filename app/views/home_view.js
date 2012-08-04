d2df2ae013ea84ed0a69ee8e4ee03d6d3bd37a21
a3gis <henri.maurer@gmail.com>
2012-08-04 13:30:21 +0200

    Init

---
+++ app/views/home_view.js
@@ -0,0 +1,19 @@
+var View = require('./view');
+var template = require('./templates/home');
+var application = require('application');
+
+module.exports = View.extend({
+	id: 'home-view',
+	template: template,
+	initialize: function() {
+		application.setTitle("Yeahhh !!")
+		this.sec = 0;
+		this.millisec = 0;
+		
+		var view = this
+	//	setInterval(function() { view.millisec += 100; if(view.millisec == 1000) { view.sec += 1; view.millisec = 0;}; view.render() }, 100)
+	},
+	getRenderData: function() {
+		return {sec: this.sec, millisec: this.millisec}
+	}
+});
